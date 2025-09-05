641fdc6f3ca83ccc284d27402a7fc72e8e85d816
Vladut Buhoci <vbu@Vladuts-MacBook-Air.local>
2025-09-05 21:06:52 +0300

    [chore] small visual tweak for Tmux status bar + improve CICD process for memory tool

--- tools/get-memory-stats/ci_build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-rm -rf ./bin/
-go clean
-go build -o ./bin/get-memory-stats
--- tools/get-memory-stats/ci_install.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-sudo ln -s $(pwd)/bin/get-memory-stats /usr/local/bin/get-memory-stats
